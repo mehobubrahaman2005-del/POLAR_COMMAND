4e1f4e52b8340e1bb76ccd502c64140eda006cb0
mehobubrahaman2005-del <mehobubrahaman2005@gmail.com>
2026-09-25 22:16:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ def login():
     # Demo Commander account
     if (
         email == "commander@polar.local"
-        and password == "polar123"
+        and password == "commander123"
     ):
 
         session["logged_in"] = True
